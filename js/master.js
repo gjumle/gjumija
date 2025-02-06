5d49eb552e2d60a99b46336e82d118e7828d9689
Leoš Gjumija <gjumle@protonmail.com>
2025-02-06 18:41:25 +0100

    index page & navbar

---
+++ js/master.js
@@ -0,0 +1,11 @@
+const setTheme = (theme) => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+};
+
+const getTheme = () => {
+    const theme = localStorage.getItem("theme");
+    theme && setTheme(theme);
+};
+
+getTheme();
